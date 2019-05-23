d30bdb7bda0728e406167f9b2abc4773f3c38bdb
JorgeReneArevalo <49214049+JorgeReneArevalo@users.noreply.github.com>
2019-05-23 11:20:19 -0500

    Add files via upload

---
+++ VALIDACIONDEUSUARIO/controladores/bienvenido.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+ <title>Crear Nuevo Usuario</title>
+    <style type="text/css" rel="stylesheet">
+    .error{
+        color: red ;
+        font-size: 30px;
+    }
+    </style>
+    
+</head>
+<body>
+
+ <?php
+
+ echo "<a href='../crear_usuario.html'>“BIENVENIDO PASASTE LAS VALIDACIONES !”</a>";
+ 
+ ?>
+ </body>
+ </html>
--- VALIDACIONDEUSUARIO/js/validaciones.js
+++ VALIDACIONDEUSUARIO/js/validaciones.js
@@ -29,14 +29,15 @@ function validarCamposObligatorios() {
             if (elemento.id == 'fechaNacimiento') {
                 document.getElementById('mensajeFechaN').innerHTML = '<br> La fecha de nacimieto esta vacio <br>'
             }
+            elemento.style.border = '1px red solid'
+            //elemento.className = 'error'
+            bandera = false
         }
-        elemento.style.border = '1px red solid'
-        elemento.className = 'error'
-        bandera = false
+
 
     }
     if (!bandera) {
-        alert('Ahora si mete bien el dedo')
+        //alert('Ahora si mete bien el dedo')
     }
     return bandera
 }
@@ -51,25 +52,37 @@ function validarCamposObligatorios() {
 //}
 
 
-function validarLetras(n){  
-    alert(n)
-    var numeros = "0123456789"
-    var letras = document.getElementById(n).value 
-    var cadena 
-   if(n== "nombres"){  
-      cadena = letras.substring(letras.length-1)
-      if(numeros.indexOf(cadena,0)!=-1){  
-         var bien = letras.substring(0,letras.length-1); 
-         document.getElementById('nombres').value= bien
-      }
-    }else if (n== "apellidos"){  
-        cadena = letras.substring(letras.length-1) 
-        if(numeros.indexOf(cadena,0)!=-1){  
-            var bien2 = letras.substring(0,letras.length-1); 
-            document.getElementById('apellidos').value= bien2
-         }
+function validarLetras(letras) {
+    var n = document.getElementById(letras.id).value
+    if (letras.id == 'nombres') {
+        var asc = n.substr(n.length - 1).charCodeAt(0)
+        if ((asc >= 65 && asc<= 90) || (asc >= 97 && asc <= 122) || asc == 32 || asc == 225 || asc == 233 || asc == 237 || asc == 243 || asc == 250 || asc == 193 || asc == 201 || asc == 205 || asc == 211 || asc == 218) {
+            var espaciosNom = n.split(" ") 
+            var total = espaciosNom.length
+            if (total> 2) {
+                document.getElementById('mensajeNombres').innerHTML = 'No es permitido mas de dos nombres'
+            }else{ 
+                document.getElementById('mensajeNombres').innerHTML = ''
+            }
+        } else {
+            document.getElementById('nombres').value = n.substr(0, n.length - 1)
+        }
+    } else if (letras.id == 'apellidos') {
+        var asc2= n.substr(n.length - 1).charCodeAt(0)
+        if ((asc2 >= 65 && asc2 <= 90) || (asc2 >= 97 && asc2 <= 122) || asc2 == 32 || asc2 == 225 || asc2 == 233 || asc2 == 237 || asc2 == 243 || asc2 == 250 || asc2 == 193 || asc2 == 201 || asc2 == 205 || asc2 == 211 || asc2 == 218) {
+            var espaciosApe = n.split(" "); 
+            var total2 = espaciosApe.length;
+            if (total2 > 2) {
+                document.getElementById('mensajeApellidos').innerHTML = 'No es permitido mas de dos Apellidos'
+            }else{  
+                document.getElementById('mensajeApellidos').innerHTML = ''
+            }
+
+        } else {
+            document.getElementById('apellidos').value = n.substr(0, n.length - 1)
+        }
     }
-} 
+}
 
 function ValidacionDeCedula() {
     var cedula = document.getElementById('cedula').value.trim();
@@ -114,32 +127,45 @@ function ValidacionDeCedula() {
     }
 }  
 
-function validarNumeros(l){ 
-    var letras = "abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
-    var numeros = document.getElementById(l).value 
-    var cadena 
-    cadena = numeros.substring(numeros.length-1)
-    if(letras.indexOf(cadena,0)!=-1){  
-      var bien = numeros.substring(0,numeros.length-1); 
-      document.getElementById(l).value= bien
-    }
-   
- } 
-
- function validarCorreo(){ 
-    var correo = document.getElementById("correo").value; 
-    var val = correo.substring(correo.length-15)  
-    var val2 = correo.substring(correo.length-11)
-    if(val != "@est.ups.edu.ec" && val2 != "@est.ups.ec" ){ 
-        for(var i = 0; i < document.forms[0].elements.length; i++){ 
-            var elemento = document.forms[0].elements[i] 
-            if(elemento.id == 'correo'){  
-            document.getElementById('mensajeCorreo').innerHTML = ("Correo Incorrecto <br>"); 
-            elemento.style.border = '1px red solid'
-            }  
+function validarNumeros(datos) {
+    var nums = document.getElementById(datos.id).value
+    if (datos.id == 'telefono') {
+        if (nums.length > 10) {
+            document.getElementById('telefono').value = nums.substr(0, nums.length - 1)
+        } else {
+            document.getElementById('mensajeTelefono').innerHTML = ''
+            var n = nums.substr(nums.length - 1).charCodeAt(0)
+            if (n >= 48 && n <= 57) {
+            } else {
+                document.getElementById('telefono').value = nums.substr(0, nums.length - 1)
+            }
         }
     }
+
 }
+
+function validarCorreo() {
+    var correo = document.getElementById("correo").value;
+    var longitud = correo.length
+    var val = correo.substring(correo.length - 15)
+    var val2 = correo.substring(correo.length - 11)
+    var arreglo = new Array(2);
+    arreglo = correo.split('@');
+    var val3 = arreglo[0]
+    console.log(val3.length)
+
+    if (val != "@est.ups.edu.ec" && val2 != "@est.ups.ec") {
+        document.getElementById('mensajeCorreo').innerHTML = ("Correo Incorrecto");
+
+    } else if (val3.length < 3) {
+        document.getElementById('mensajeCorreo').innerHTML = "El correo debe de  tener mas de tres caracteres";
+    } else if (longitud > 70) {
+        document.getElementById('mensajeCorreo').innerHTML = ("Correo Incorrecto");
+    } else {
+        document.getElementById('mensajeCorreo').innerHTML = "";
+    }
+}
+
 
 function valFecha(datos) {
     var fecha = document.getElementById(datos.id).value
